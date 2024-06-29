d716cfe5b034350358fcdca086a2e758795019e5
fagnerdev2024 <fagnertavaresdemenezes2024@gmail.com>
2024-06-29 00:22:45 -0300

    first commit

---
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,20 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(
+
+        @JsonAlias("Title")
+        String tituloooo,
+
+        @JsonAlias("totalSeasons")
+        Integer totalTemporadas,
+
+        @JsonAlias("imdbRating")
+        String avaliacao) {
+
+
+}
---
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -0,0 +1,8 @@
+package br.com.alura.screenmatch.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+}
