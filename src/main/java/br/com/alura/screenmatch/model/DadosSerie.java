3757ce8ec39dba062d76ac92b15acf24e2dbaf1c
fagnerdev2024 <fagnertavaresdemenezes2024@gmail.com>
2024-07-01 20:39:13 -0300

    meu ajuste

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -7,13 +7,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(
 
-        @JsonAlias("Titleeee")
+        @JsonAlias("Title")
         String tituloooo,
 
-        @JsonAlias("totalSeasonssssssss")
+        @JsonAlias("totalSeasons")
         Integer totalTemporadas,
 
-        @JsonAlias("imdbRatingggggggg")
+        @JsonAlias("imdbRating")
         String avaliacao) {
 
 
